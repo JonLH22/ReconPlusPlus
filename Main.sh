cae7d9bce6e6ea2fb9ccec6ea6a9cf20b13376d8
BlackCat1122 <jonlharvey@hotmail.com>
2024-02-25 22:33:38 -0600

    Update Main.sh
    Added functions for each program

--- Main.sh
+++ Main.sh
@@ -24,28 +24,45 @@ if [ ! -d "$scan_path" ]; then
     mkdir $domain 
 fi 
 
-#Subfinder
-echo -e "${RED} [+] Launching subfinder ... ${RESET}"
-subfinder -d $domain > $subdomain_path/found.txt 
 
-#assetfinder
-echo -e "${RED} [+] Launching assetfinder ... ${RESET}"
-assetfinder $domain | grep $domain >> $subdomain_path/found.txt
+#Subfinder function
+function subfinder {
+    echo -e "${RED} [+] Launching subfinder ... ${RESET}"
+    subfinder -d $domain > $subdomain_path/found.txt 
+}
+subfinder
 
-#amass
-sudo apt-get install amass -y
-echo -e "${RED} [+] Launching amass ... ${RESET}"
-amass enum -d $domain >> $subdomain_path/found.txt
+#assetfinder function
+function assetfinder {
+    echo -e "${RED} [+] Launching assetfinder ... ${RESET}"
+    assetfinder $domain | grep $domain >> $subdomain_path/found.txt
+}
+assetfinder
 
-echo -e "${RED} [+] Finding alive subdomains ... ${RESET}"
-cat $subdomain_path/found.txt | grep $domain | sort -u  | httpprobe -prefer-https | grep https | sed 's/https\?:\///' | tee -a $subdomain_path/alive.txt 
+#amass function
+function amass {
+    sudo apt-get install amass -y
+    echo -e "${RED} [+] Launching amass ... ${RESET}"
+    amass enum -d $domain >> $subdomain_path/found.txt
+}
+amass
 
-#gowitness
-sudo apt-get install gowitness -y
-echo -e "${RED} [+] Taking screenshots of alive subdomains ... ${RESET}"
-gowitness file -f $subdomain_path/alive.txt -P $screenshot_path/ --no-http
-
-#nmap
-echo -e "${RED} [+] Running nmap on alive subdomains ... ${RESET}"
-nmap -iL $subdomain_path/alive.txt -T4 -A -p- -oN $scan_path/nmap.txt
-
+#httpprobe function
+function httpprobe {
+    echo -e "${RED} [+] Finding alive subdomains ... ${RESET}"
+    cat $subdomain_path/found.txt | grep $domain | sort -u  | httpprobe -prefer-https | grep https | sed 's/https\?:\///' | tee -a $subdomain_path/alive.txt 
+}
+httpprobe
+#gowitness function
+function gowitness {
+    sudo apt-get install gowitness -y
+    echo -e "${RED} [+] Taking screenshots of alive subdomains ... ${RESET}"
+    gowitness file -f $subdomain_path/alive.txt -P $screenshot_path/ --no-http
+}
+gowitness
+#nmap function 
+function nmap {
+    echo -e "${RED} [+] Running nmap on alive subdomains ... ${RESET}"
+    nmap -iL $subdomain_path/alive.txt -T4 -A -p- -oN $scan_path/nmap.txt
+}
+nmap
